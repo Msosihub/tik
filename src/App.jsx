3cfd87c231b0bb9fb49681d731a91d3070a56ca1
Msosihub <msosihub@gmail.com>
2024-03-31 20:24:34 +0300

    winner

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,54 @@
 import Player from './components/Player.jsx'
 import GameBoard from './components/GameBoard.jsx'
+import Log from './components/Log.jsx'
+import { useState } from 'react'
+import { WINNING_COMBINATIONS } from './winning_combinations.js'
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+]
+
+function deriveActivePlayer(gameTurns){
+  let currentPlayer = 'X';
+
+      if(gameTurns.length > 0 && gameTurns[0].player == 'X'){
+        currentPlayer = 'O';
+      }
+
+  return currentPlayer;
+}
 
 function App() {
-  
+  const [gameTurn, setGameTurn] =useState([]);
+  // const [activePlayer, setActivePlayer] =useState('X');
+  const activePlayer = deriveActivePlayer(gameTurn);
+
+  let gameBoard = initialGameBoard;
+
+  for( const turn of gameTurn){
+
+      const { square, player } = turn;
+      const { row, col } = square;
+
+      gameBoard[row][col] = player
+  }
+
+  function handleSelectSquare(rowIndex, colIndex){
+    // setActivePlayer((curentPlayer) => curentPlayer === 'X' ? 'O' : 'X');
+
+    setGameTurn((prevTurn)=>{
+      let currentPlayer = deriveActivePlayer(prevTurn);
+
+      const updatedTurn = [
+        {square : {row : rowIndex, col : colIndex}, player: currentPlayer},
+        ...prevTurn
+      ];
+
+      return updatedTurn;
+    });
+  }
 
   return (
     <main>
@@ -12,14 +58,14 @@ function App() {
     </header>
 
     <div id="game-container">
-      <ol id="players">
-        <Player initialName="Player 1" symbol="X" />
-        <Player initialName="Player 2" symbol="0" />
+      <ol id="players" className="highlight-player">
+        <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
+        <Player initialName="Player 2" symbol="0" isActive={activePlayer === 'O'}/>
         
       </ol>
-       <GameBoard/>
+       <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
     </div>
-
+    <Log turns={gameTurn}/>
     </main>
   )
 }
