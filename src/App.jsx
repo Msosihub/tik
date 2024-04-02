28865ba670b6f8c4158205a2e8eb90f753611cd6
Msosihub <msosihub@gmail.com>
2024-04-02 12:33:01 +0300

    bord

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import GameBoard from './components/GameBoard.jsx'
 import Log from './components/Log.jsx'
 import { useState } from 'react'
 import { WINNING_COMBINATIONS } from './winning_combinations.js'
+import GameOver from './components/GameOver.jsx'
 
 const initialGameBoard = [
   [null, null, null],
@@ -15,7 +16,7 @@ function deriveActivePlayer(gameTurns){
 
       if(gameTurns.length > 0 && gameTurns[0].player == 'X'){
         currentPlayer = 'O';
-      }
+      } 
 
   return currentPlayer;
 }
@@ -34,6 +35,20 @@ function App() {
 
       gameBoard[row][col] = player
   }
+  let winner;
+
+  for(const combination of WINNING_COMBINATIONS){
+    const fSS = gameBoard[combination[0].row][combination[0].column]
+    const sSS = gameBoard[combination[1].row][combination[1].column]
+    const tSS = gameBoard[combination[2].row][combination[2].column]
+
+    if (fSS && fSS === sSS & fSS === tSS){
+      winner = fSS;
+    }
+  
+  }
+
+  const hasDraw = gameTurn.length == 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex){
     // setActivePlayer((curentPlayer) => curentPlayer === 'X' ? 'O' : 'X');
@@ -63,6 +78,7 @@ function App() {
         <Player initialName="Player 2" symbol="0" isActive={activePlayer === 'O'}/>
         
       </ol>
+      {(winner || hasDraw) && <GameOver winner={winner}/>}
        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
     </div>
     <Log turns={gameTurn}/>
